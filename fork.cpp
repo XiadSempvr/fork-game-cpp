b9f094f056a63bdeaf9b20f5441af5cf6d3243c0
kekaritomene <momus@kekaritomenes-MacBook-Air.local>
2026-06-13 19:27:54 -0300

    implementacao de máscara para palavras

--- fork.cpp
+++ fork.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <random>
+#include <vector>
 
 auto sort_rand_index(int arr_size) -> int{
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dist(0, arr_size - 1);
     return dist(gen);
+}
+
+auto mask(std::string word, char mask) -> std::string{
+    for(auto& c : word) c = mask;
+    return word;
 }
 
 int main(){
@@ -18,7 +24,8 @@ int main(){
         "pessoa"
     };
 
-    std::cout << arr[sort_rand_index(std::size(arr))] << std::endl;
+    std::string word = arr[sort_rand_index(std::size(arr))];
+    std::cout<<"Word: "<<mask(word, '*')<<std::endl;
     
     return 0;
 }
