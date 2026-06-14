e7cb208f853a685333f1f5bb31526ceab9acccf9
kekaritomene <momus@kekaritomenes-MacBook-Air.local>
2026-06-14 11:06:30 -0300

    Pseudo leitura de arquivos com ifstream: feat, melhoria

--- fork.cpp
+++ fork.cpp
@@ -2,11 +2,35 @@
 #include <random>
 #include <vector>
 
-auto sort_rand_index(int arr_size) -> int{
+auto rand_word(const std::string& f) -> std::string {
+    std::ifstream file(f);
+
+    if (!file) {
+        throw std::runtime_error("File doesn't exist");
+    }
+
+    std::vector<std::string> words;
+    std::string line;
+
+    while (std::getline(file, line)) {
+        if (!line.empty()) {
+            words.push_back(line);
+        }
+    }
+
+    if (words.empty()) {
+        throw std::runtime_error("Empty file");
+    }
+
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<> dist(0, arr_size - 1);
-    return dist(gen);
+
+    std::uniform_int_distribution<> dist(
+        0,
+        words.size() - 1
+    );
+
+    return words[dist(gen)];
 }
 
 auto mask(std::string word, char mask) -> std::string{
@@ -15,17 +39,6 @@ auto mask(std::string word, char mask) -> std::string{
 }
 
 int main(){
-    std::string arr[] = {
-        "banana",
-        "uva",
-        "maca",
-        "peira",
-        "anima",
-        "pessoa"
-    };
-
-    std::string word = arr[sort_rand_index(std::size(arr))];
-    std::cout<<"Word: "<<mask(word, '*')<<std::endl;
-    
+    std::cout<<rand_word("../testes_com_arquivos/sla.txt")<<std::endl;
     return 0;
 }
